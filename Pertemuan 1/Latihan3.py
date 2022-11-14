44dd1b5d1c18782517a3716d1e2361c3537412f8
Raka Pradana <117451926+rakaprdana@users.noreply.github.com>
2022-11-14 20:37:02 +0700

    Add files via upload

---
+++ Pertemuan 1/Latihan3.py
@@ -0,0 +1,23 @@
+#Variabel adalah tempat menyimpan data
+
+#Menaru/ assignment nilai
+a=10
+x=5
+panjang =20000
+print("Nilai x ", a)
+print("Nilai y ", x)
+print("Nilai panjang ", panjang)
+
+#penamaan
+nilai_y=15 
+juta10=10000000
+nilaiZ= 17.5
+
+#pemanggilan kedua
+print ("Nilai a=",a)
+a =7
+print ("Nilai a=",a)
+
+# assignment indirect
+b=a
+print("Nilai b=", b)
---
+++ Pertemuan 1/Latihan4.py
@@ -0,0 +1,31 @@
+#tipe data boolean
+print(True)
+#tipe data string
+print("Ayo belajar python")
+print("Belajar python sangat mudah")
+
+#tipe data integer
+print(20)
+
+#tipe data float
+print(3.14)
+
+#tipe data hexadecimal
+print("9a")
+
+#tipe data complex
+print("5j")
+
+#tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga","emapt","lima"])
+
+#tipe data tuple
+print((1,2,3,4,5))
+print(("satu","dua","tiga","empat","lima"))
+
+#tipe data Dictionary
+print({"nama": "Budi", 'umur':20})
+biodata={"nama": "Budi", 'umur':20}
+print(biodata)
+print(type(biodata))
