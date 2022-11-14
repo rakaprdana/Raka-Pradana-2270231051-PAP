5b4566a486e6f1761f7b70373e10ca60b199c074
Raka Pradana <117451926+rakaprdana@users.noreply.github.com>
2022-11-14 20:37:32 +0700

    Add files via upload

---
+++ Pertemuan 2/Latihan7.py
@@ -0,0 +1,41 @@
+#OPERATOR ARITMATIKA
+#Penjumlahan
+apel = 7
+jeruk = 9
+buah = apel + jeruk #
+print("1. Jadi buah yang anda miliki ada", buah)
+
+#Pengurangan
+hutang = 10000
+bayar = 5000
+sisaHutang = hutang - bayar
+print("2. Sisa hutang Anda adalah ", sisaHutang)
+
+#Perkalian
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print("3. Jadi hasil dari panjang x lebar adalah",luas)
+
+#Pembagian
+kue = 16
+anak = 4
+kuePerAnak = kue / anak
+print("4. Setiap anak akan mendapatkan bagian kue sebanyak ", kuePerAnak)
+
+#Sisa Bagi / Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print("5. Sisa bagi dari bilangan ", bilangan1, " dan ", bilangan2, " adalah ", hasil)
+
+
+#Pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print("6. Jadi hasil dari perpangkatan adalah",hasilPangkat)
+
+#Pembagian Bulat
+print("7. Hasil pembulatan", 10//3)
+#10 dibagi 3 adalah 3.3333. Karena dibulatkan maka akan menghasilkan nilai 3
