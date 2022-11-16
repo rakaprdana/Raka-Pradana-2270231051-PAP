5b8770023e6745ad23d71c09e46790422076bba3
Raka Pradana <117451926+rakaprdana@users.noreply.github.com>
2022-11-16 11:19:20 +0700

    Add files via upload

---
+++ Raka Pradana-2270231051-PAP/Pertemuan 1/Latihan6.py
@@ -0,0 +1,16 @@
+# input user
+# data yang dimasukan pasti string
+data =input("Masukan data: ")
+print("data = ",data,",type =",type(data))
+print("\n")
+
+# jika kita ingin mengambil int, maka
+angka1 = float(input("masukan angka desimal: "))
+angka = int(input("masukan angka: "))
+print("data = ",angka1,",type =",type(angka1))
+print("data = ",angka,",type =",type(angka))
+print("\n")
+
+#bagaimana dengan boolean
+biner = bool(int(input("masukan nilai boolean: ")))
+print("data = ",biner,",type =",type(biner))
